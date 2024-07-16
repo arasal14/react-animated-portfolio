3d5875e09cf05ff9c00536dbe3095f5eb0b2e112
arasal14 <106369647+arasal14@users.noreply.github.com>
2024-07-16 16:01:37 +0530

    animated-first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,13 @@ export const Navbar = () => {
       <div className="flex flex-shrink-0 items-center">
         <img className="w-10" src={logo} alt="logo"></img>
       </div>
-      <div className="flex mx-auto justify-center gap-4">
+      {/* <div className="flex mx-auto justify-center gap-4">
         <h1 className="cursor-pointer">Home</h1>
         <h1 className="cursor-pointer">About</h1>
         <h1 className="cursor-pointer">Technologies</h1>
         <h1 className="cursor-pointer">Education</h1>
         <h1 className="cursor-pointer">Project</h1>
-      </div>
+      </div> */}
       <div className="m-4 flex items-center justify-center gap-4 text-xl">
         <FaLinkedin />
         <FaGithub />
